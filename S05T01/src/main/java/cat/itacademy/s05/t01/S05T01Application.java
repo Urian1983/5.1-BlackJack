5e96a884a73b8fc4ec33c2baafd5b7e08022614d
Urian1983 <urianr@gmail.com>
2026-01-15 11:35:35 +0100

    repairing some issues

--- S05T01/src/main/java/cat/itacademy/s05/t01/S05T01Application.java
+++ S05T01/src/main/java/cat/itacademy/s05/t01/S05T01Application.java
@@ -1,3 +1,5 @@
+package cat.itacademy.s05.t01;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- S05T01/src/main/java/cat/itacademy/s05/t01/repository/RankingRepository.java
+++ S05T01/src/main/java/cat/itacademy/s05/t01/repository/RankingRepository.java
@@ -2,8 +2,10 @@ package cat.itacademy.s05.t01.repository;
 
 import cat.itacademy.s05.t01.domain.sql.PlayerRanking;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
+@Repository
 public interface RankingRepository extends R2dbcRepository<PlayerRanking,Long> {
     Flux<PlayerRanking> findAllByOrderByGamesWonDesc();
 }
